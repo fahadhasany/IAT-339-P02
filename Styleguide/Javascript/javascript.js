f577c8ab8a18db283e67bd5ca4f55d90ad3f2280
Fahad <fhasany@sfu.ca>
2019-02-24 19:07:52 -0800

    Commented out Viewport JS  Code

--- Styleguide/Javascript/javascript.js
+++ Styleguide/Javascript/javascript.js
@@ -14,7 +14,7 @@ function decreaseValue() {
 }
 
 //For Mobile
-var siteWidth = 1280;
-var scale = screen.width /siteWidth
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+//var siteWidth = 1280;
+//var scale = screen.width /siteWidth
+//
+//document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
